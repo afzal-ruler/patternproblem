c068f0af5750e1ffd09c71a9def18ef0c9cc1f42
afzal-ruler <afzal931313@gmail.com>
2024-03-28 12:51:58 +0530

    Rectangle

--- Pattern.java
+++ Pattern.java
@@ -1,8 +1,31 @@
 package PatterProblem;
 
+import java.util.Scanner;
+
 public class Pattern {
 
+
+    public static void rectangle(int row, int column){
+
+        for(int i=1; i<=row; i++){
+            for (int j=0; j<=column; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Afzal");
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the row value: ");
+        int row = sc.nextInt();
+        System.out.print("Enter the column value: ");
+        int column = sc.nextInt();
+
+        sc.close();
+
+        rectangle(row, column);
+
     }
 }
