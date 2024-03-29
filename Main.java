06c86ae93b3e41b0fb6cb79e3ac13b608c714707
afzal-ruler <afzal931313@gmail.com>
2024-03-29 15:00:50 +0530

    complete pattern prroblem

--- Main.java
+++ Main.java
@@ -3,19 +3,23 @@ package PatterProblem;
 public class Main {
     public static void main(String[] args) {
         Pattern pr = new Pattern();
-        // pr.rectangle(9, 9);
-        // pr.invertedNumberHalfPyramid(10);
-        // pr.borderRectangle(9, 9);
-        // pr.floydsNumberTriangle(9);
-        // pr.halfPyramid(9);
-        // pr.invertedHalfPyramid(9);
-        // pr.numberHalfPyramid(9);
-        // pr.rotatedHalfPyramid(9);
-        // pr.zeroOneTriangle(9);
-        // pr.butterfly(5);
+        pr.rectangle(9, 9);
+        pr.invertedNumberHalfPyramid(10);
+        pr.borderRectangle(9, 9);
+        pr.floydsNumberTriangle(9);
+        pr.halfPyramid(9);
+        pr.invertedHalfPyramid(9);
+        pr.numberHalfPyramid(9);
+        pr.rotatedHalfPyramid(9);
+        pr.zeroOneTriangle(9);
+        pr.butterfly(5);
+        pr.boundryRhombus(9);
+        pr.boundryRhombus(9);
+        pr.numberPyramid(9);
+        pr.pallindromePyramid(0);
+        pr.pyramid(10);
+        pr.rhombus(9);
 
-        // pr.pyramid(10);
-
-        pr.pyramid(9);
+        pr.dimondPatter(9);
     }
 }
